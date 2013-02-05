05c38bc55ead71428d5a2ddc0b27c963cc33eeee
Jordan Hatch <jordan@jordanh.net>
2013-02-05 14:37:14 +0000

    Set up test helper for Carrierwave
    Use separate upload directory
    Clean the directory after each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,22 +18,28 @@ AssetUploader.enable_processing = false
 RSpec.configure do |config|
   config.mock_with :mocha
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.after(:all) do
+    clean_upload_directory!
+  end
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = "random"
+end
+
+CarrierWave::Uploader::Base.descendants.each do |klass|
+  next if klass.anonymous?
+  klass.class_eval do
+    def cache_dir
+      "#{Rails.root}/spec/support/uploads/tmp"
+    end
+
+    def store_dir
+      "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{model.id}"
+    end
+  end
+end
+
+def clean_upload_directory!
+  FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads/[^.]*"])
 end
