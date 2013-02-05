bc0a15248e3e51a411a7168c2254e65df1b5665c
Jordan Hatch <jordan@jordanh.net>
2013-02-05 11:55:31 +0000

    Add asset uploader
    Stock Carrierwave uploader, with the `store_dir`
    and `cache_dir` paths changed

---
+++ app/uploaders/asset_uploader.rb
@@ -0,0 +1,47 @@
+# encoding: utf-8
+
+class AssetUploader < CarrierWave::Uploader::Base
+
+  storage :file
+
+  def store_dir
+    "#{Rails.root}/uploads/assets/#{model.id}"
+  end
+
+  def cache_dir
+    "/tmp/uploads"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded:
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :scale => [50, 50]
+  # end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  # def extension_white_list
+  #   %w(jpg jpeg gif png)
+  # end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
+
+end
