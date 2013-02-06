d068b4c4fae1a7ec0439caab0b7def7bc9e28dda
Jordan Hatch <jordan@jordanh.net>
2013-02-06 17:14:04 +0000

    Prevent access to the application unless authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  include GDS::SSO::ControllerMethods
+  before_filter :authenticate_user!
+  before_filter :require_signin_permission!
+
   private
     def error_404
       error 404, "not found"
