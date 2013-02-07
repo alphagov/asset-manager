902dd97b8e0e6dc46347bb0b70aaedc1e0b969b8
Jordan Hatch <jordan@jordanh.net>
2013-02-07 16:46:31 +0000

    Tidy development gds-sso

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,14 +1,8 @@
 GDS::SSO.config do |config|
   config.user_model   = 'ReadOnlyUser'
 
-  # set up ID and Secret in a way which doesn't require it to be checked in to source control...
   config.oauth_id     = ENV['OAUTH_ID']
   config.oauth_secret = ENV['OAUTH_SECRET']
 
-  # optional config for location of signonotron2
-  config.oauth_root_url = "http://localhost:3001"
-
-  # optional config for API Access (requests which accept application/json)
-  config.basic_auth_user = 'api'
-  config.basic_auth_password = 'secret'
+  config.oauth_root_url = Plek.current.find("signon")
 end
