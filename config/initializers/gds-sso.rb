e4737ae5ae57bdd6652e0b354e02c17f76271423
Jordan Hatch <jordan@jordanh.net>
2013-02-08 16:19:06 +0000

    Store the user in the local database
    ReadOnlyUser is nasty and doesn't support 
    permissions.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,5 +1,5 @@
 GDS::SSO.config do |config|
-  config.user_model   = 'ReadOnlyUser'
+  config.user_model   = 'User'
 
   config.oauth_id     = ENV['OAUTH_ID']
   config.oauth_secret = ENV['OAUTH_SECRET']
