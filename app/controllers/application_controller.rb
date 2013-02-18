d8ee0f0c7f1eca106802b6e8be63014deab22e58
Kushal Pisavadia <kushi.p@gmail.com>
2013-02-18 11:50:52 +0000

    Add extra newline between include and before_filter blocks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include GDS::SSO::ControllerMethods
+
   before_filter :authenticate_user!
   before_filter :require_signin_permission!
 
