b045f487e95e3e4da3545b5cc919aadc6d13b322
Kushal Pisavadia <kushi.p@gmail.com>
2013-02-19 12:23:03 +0000

    Correct file headers should be set for send_file requests

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -5,5 +5,9 @@ class FilesController < ApplicationController
   def download
     @asset = Asset.find(params[:id])
     error_404 if @asset.nil? || @asset.file.file.identifier != params[:filename]
+
+    respond_to do |format|
+      format.any { send_file(@asset.file.path, :x_sendfile => true) }
+    end
   end
 end
---
+++ spec/controllers/files_controller_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe FilesController do
+  before(:each) do
+    login_as_stub_user
+  end
+
+  describe "GET files" do
+    describe "HTTP headers" do
+      before(:each) do
+        @asset = FactoryGirl.create(:asset)
+        get :download, :id => @asset.id, :filename => @asset.file.identifier
+      end
+
+      it "should response with 200" do
+        response.status.should == 200
+      end
+
+      it "should have the correct content type" do
+        response.headers["Content-Type"].should == "image/png"
+      end
+
+      it "should set the X-Sendfile header" do
+        response.headers["X-Sendfile"] == @asset.file.identifier
+      end
+    end
+  end
+end
