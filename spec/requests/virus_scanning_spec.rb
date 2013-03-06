c47a009374761d523263457046dbb7fed5704471
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-03-06 15:00:54 +0000

    Integration tests for virus scanning (pending)

---
+++ spec/requests/virus_scanning_spec.rb
@@ -0,0 +1,62 @@
+require "spec_helper"
+
+
+describe "Virus scanning of uploaded images" do
+
+  specify "uploading a clean asset, and seeing it available after virus scanning", :pending => true do
+    post "/assets", :asset => { :file => load_fixture_file("lorem.txt") }
+    response.status.should == 201
+
+    @asset = Asset.last
+
+    asset_details = JSON.parse(response.body)
+    asset_details["id"].should =~ %r{http://www.example.com/assets/#{@asset.id}}
+
+    get "/media/#{@asset.id}/lorem.txt"
+    response.status.should == 404
+
+    # Run virus scanning
+
+    get "/media/#{@asset.id}/lorem.txt"
+    response.status.should == 200
+
+    expected = File.read(fixture_file_path("lorem.txt"))
+    response.body.should == expected
+  end
+
+  # Extension to UploadedFile to represent an uploaded virus
+  # without having to have a 'virus' committed into source control.
+  #
+  # This is using the EICAR test virus (details: http://www.eicar.org/86-0-Intended-use.html)
+  class UploadedVirus < Rack::Test::UploadedFile
+    def initialize()
+      @content_type = "text/plain"
+      @original_filename = 'eicar.com'
+
+      @tempfile = Tempfile.new(@original_filename)
+      @tempfile.set_encoding(Encoding::BINARY) if @tempfile.respond_to?(:set_encoding)
+
+      @tempfile.write "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STAN"
+      @tempfile.write "DARD-ANTIVIRUS-TEST-FILE!$H+H*"
+      @tempfile.rewind
+    end
+  end
+
+  specify "uploading an infected asset, and not seeing it available after virus scanning", :pending => true do
+    post "/assets", :asset => { :file => UploadedVirus.new }
+    response.status.should == 201
+
+    @asset = Asset.last
+
+    asset_details = JSON.parse(response.body)
+    asset_details["id"].should =~ %r{http://www.example.com/assets/#{@asset.id}}
+
+    get "/media/#{@asset.id}/eicar.com"
+    response.status.should == 404
+
+    # Run virus scanning
+
+    get "/media/#{@asset.id}/eicar.com"
+    response.status.should == 404
+  end
+end
--- spec/support/file_helpers.rb
+++ spec/support/file_helpers.rb
@@ -7,7 +7,11 @@ RSpec.configuration.include FileHelpers, :type => :model
 
 module ControllerFileHelpers
   def load_fixture_file(path)
-    Rack::Test::UploadedFile.new Rails.root.join("spec", "fixtures", "files", path)
+    Rack::Test::UploadedFile.new fixture_file_path(path)
+  end
+
+  def fixture_file_path(filename)
+    Rails.root.join("spec", "fixtures", "files", filename)
   end
 end
 RSpec.configuration.include ControllerFileHelpers, :type => :controller
