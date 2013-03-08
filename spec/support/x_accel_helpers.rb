5a12310bff712af04cfbcb429192d6b4d327fa95
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-03-08 16:06:31 +0000

    Remove cruft that got committed by mistake

--- spec/support/x_accel_helpers.rb
+++
@@ -1,9 +0,0 @@
-#module XAccelHelpers
-  #def x_accel_get(url)
-    #get url, nil, {
-      #"HTTP_X_SENDFILE_TYPE" => "X-Accel-Redirect",
-      #"HTTP_X_ACCEL_MAPPING" => "#{Rails.root}/tmp/test_uploads/assets/=/raw/",
-    #}
-  #end
-#end
-#RSpec.configuration.include XAccelHelpers, :type => :request
