cbe5a2584e7ecf43f0602ab275d0214ea303e9ab
pezholio <pezholio@gmail.com>
2013-07-24 08:56:18 +0100

    Add Carrierwave config

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,9 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider           => 'Rackspace',
+    :rackspace_username => ENV['RACKSPACE_USERNAME'],
+    :rackspace_api_key  => ENV['RACKSPACE_API_KEY'],
+    :rackspace_auth_url => ENV['RACKSPACE_API_ENDPOINT']
+  }
+  config.fog_directory = ENV['RACKSPACE_DIRECTORY_CONTAINER']
+end
