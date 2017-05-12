271f620b0a06bc84161300125db916b516e372a5
Tijmen Brommet <tijmen@gmail.com>
2017-05-12 14:07:11 +0100

    Maybe this

--- spec/support/delayed_job.rb
+++ spec/support/delayed_job.rb
@@ -1,6 +1,6 @@
 module DelayedJobHelpers
   def run_all_delayed_jobs
-    Delayed::Worker.new(:exit_on_complete => true, :quiet => true).start
+    Delayed::Worker.new(:exit_on_complete => true, :quiet => false).start
   end
 end
 
