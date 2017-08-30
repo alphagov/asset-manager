e8d71b25e9e19d1cb26c93ec8dd640e06c6bd3ce
James Mead <james@floehopper.org>
2017-08-30 16:21:36 +0100

    Fix some Rubocop offences & warnings in AWS config
    I was seeing the following locally:
    
    * Layout/IndentationWidth
    * Layout/ElseAlignment
    * Lint/EndAlignment
    
    For some reason these were not picked up by the Jenkins CI build of the
    master branch. I plan to investigate why this didn't happen separately.

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,8 @@
 AssetManager.aws_s3_bucket_name = if Rails.env.production?
-  ENV.fetch('AWS_S3_BUCKET_NAME')
-else
-  ENV['AWS_S3_BUCKET_NAME']
-end
+                                    ENV.fetch('AWS_S3_BUCKET_NAME')
+                                  else
+                                    ENV['AWS_S3_BUCKET_NAME']
+                                  end
 
 AssetManager.aws_s3_use_virtual_host = ENV['AWS_S3_USE_VIRTUAL_HOST'].present?
 
