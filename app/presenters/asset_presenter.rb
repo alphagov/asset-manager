f88bf116208c401f34ef90cac4afeafbe30b0eac
James Mead <james@floehopper.org>
2017-09-14 18:30:17 +0100

    Simplify AssetPresenter#as_json by using Asset#content_type

--- app/presenters/asset_presenter.rb
+++ app/presenters/asset_presenter.rb
@@ -11,15 +11,9 @@ class AssetPresenter
       },
       id: @view_context.asset_url(@asset.id),
       name: @asset.file.file.identifier,
-      content_type: asset_mime_type.to_s,
+      content_type: @asset.content_type,
       file_url: "#{Plek.new.asset_root}/media/#{@asset.id}/#{@asset.file.file.identifier}",
       state: @asset.state,
     }
-  end
-
-private
-
-  def asset_mime_type
-    MIME::Types.type_for(@asset.file.current_path).first
   end
 end
