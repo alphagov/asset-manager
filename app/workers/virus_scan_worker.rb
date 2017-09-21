4a7d15efc7bbdd678716e346070c9fbebcd5cbbe
Chris Lowis <chris.lowis@gmail.com>
2017-09-21 10:39:44 +0100

    Move Asset#scan_for_viruses to Sidekiq worker
    There is no change in behaviour if we move the implementation of this
    method from Asset into a sidekiq worker and call it
    synchronously (with `perform`). In a subsequent commit we'll call this
    method asynchronously (on the sidekiq queue) but we'll need to leave
    Asset#scan_for_viruses in place in this form in case there are still
    any jobs on the delayed_job queue when this code is deployed.
    
    We'll be able to remove this method later when there are no jobs on
    the delayed_job queue.

---
+++ app/workers/virus_scan_worker.rb
@@ -0,0 +1,18 @@
+require 'virus_scanner'
+
+class VirusScanWorker
+  include Sidekiq::Worker
+
+  def perform(asset)
+    scanner = VirusScanner.new(asset.file.current_path)
+    if scanner.clean?
+      asset.scanned_clean
+    else
+      Airbrake.notify_or_ignore(VirusScanner::InfectedFile.new, error_message: scanner.virus_info, params: { id: asset.id, filename: asset.filename })
+      asset.scanned_infected
+    end
+  rescue => e
+    Airbrake.notify_or_ignore(e, params: { id: asset.id, filename: asset.filename })
+    raise
+  end
+end
