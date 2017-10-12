02a051d8042b16c07c401e7665fa8c27dc116c6b
James Mead <james@floehopper.org>
2017-10-12 18:07:12 +0100

    Pull-up proxy_to_s3_via_nginx method into controller superclass
    In order to make this functionality available to the
    `WhitehallMediaController`.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -34,16 +34,6 @@ protected
     proxy_to_s3_via_nginx || params[:proxy_to_s3_via_nginx].present?
   end
 
-  def proxy_to_s3_via_nginx(asset)
-    url = Services.cloud_storage.presigned_url_for(asset, http_method: request.request_method)
-    headers['X-Accel-Redirect'] = "/cloud-storage-proxy/#{url}"
-    headers['ETag'] = %{"#{asset.etag}"}
-    headers['Last-Modified'] = asset.last_modified.httpdate
-    headers['Content-Disposition'] = AssetManager.content_disposition.header_for(asset)
-    headers['Content-Type'] = asset.content_type
-    render nothing: true
-  end
-
   def filename_current?
     asset.filename == params[:filename]
   end
