4a1646ff13d0464e728eeceed5e0a83a9b212539
James Mead <james@floehopper.org>
2017-11-01 15:13:33 +0000

    Add Rake task to trigger replication for all assets
    S3 objects for some assets were added to the S3 bucket *before*
    replication was enabled on the bucket. Enabling replication does not
    automatically replicate these S3 objects, so we need to do this
    manually.
    
    The simplest way to achieve this seems to be to modifiy the metadata
    on the S3 object. This feels better than manually syncing/copying the
    relevant S3 objects to the target bucket, because it uses the same
    mechanism (replication) to sync to the target bucket with all the same
    users, roles & permissions.
    
    One possible downside of this approach is that the Last-Modified
    timestamp updated when the metadata on an S3 object is modified.
    However, at the moment, we're not making use of this metadata - the
    Rails app sets a Last-Modified response header using a value stored in
    the database.
    
    Another possible downside is that the new metadata entry will be sent as
    a response header when the S3 object is requested. However, the worker
    immediately removes the new metadata entry, so this will only ever be a
    very transient issue and in any case, the extra response header
    shouldn't cause any problems.
    
    Note also that I've extracted a method to remove the duplication between
    the new Rake task and the existing Rake task and to avoid a Rubocop
    violation due to the length of the block withint the Rake namespace!

---
+++ app/workers/asset_trigger_replication_worker.rb
@@ -0,0 +1,18 @@
+class AssetTriggerReplicationWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'low_priority'
+
+  KEY = 'replication-triggered-at'.freeze
+
+  def initialize(cloud_storage: Services.cloud_storage)
+    @cloud_storage = cloud_storage
+  end
+
+  def perform(asset_id)
+    asset = Asset.find(asset_id)
+    if @cloud_storage.exists?(asset)
+      @cloud_storage.add_metadata_to(asset, key: KEY, value: Time.now.httpdate)
+      @cloud_storage.remove_metadata_from(asset, key: KEY)
+    end
+  end
+end
