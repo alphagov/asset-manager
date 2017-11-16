75c7864e34a5765b98c04fc543eff1a38465b83b
Daniel Burnley <dan@danops.uk>
2017-11-16 14:30:37 +0000

    Run rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 # Pick the frameworks you want:
 # require "active_model/railtie"
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 
 module AssetManager
   class Application < Rails::Application
+    config.load_defaults 5.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
