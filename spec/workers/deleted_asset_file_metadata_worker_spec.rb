c4a7604b499a44736e6e1d59a30643205e6dc9f3
James Mead <james@floehopper.org>
2017-11-20 13:50:35 +0000

    Add worker to store file metadata values for deleted asset
    I'm about to add a new Rake task to store attributes generated from file
    metadata in the database for assets which have been marked as deleted. I
    contemplated re-using the existing AssetFileMetadataWorker, but given
    that all of the "low priority" workers are associated with one-off Rake
    tasks, I decided to add a new worker instead.

---
+++ spec/workers/deleted_asset_file_metadata_worker_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe DeletedAssetFileMetadataWorker, type: :worker do
+  subject(:worker) { described_class.new }
+
+  let(:asset) { FactoryGirl.create(:asset) }
+
+  before do
+    asset.destroy
+  end
+
+  it 'sets Asset#etag to Asset#etag_from_file in database' do
+    asset.set(etag: nil)
+
+    worker.perform(asset.id.to_s)
+
+    expect(asset.reload.etag).to eq(asset.etag_from_file)
+  end
+
+  it 'sets Asset#last_modified to Asset#last_modified_from_file in database' do
+    asset.set(last_modified: nil)
+
+    worker.perform(asset.id.to_s)
+
+    expect(asset.reload.last_modified).to be_within_a_millisecond_of(asset.last_modified_from_file)
+  end
+
+  it 'sets Asset#md5_hexdigest to Asset#md5_hexdigest_from_file in database' do
+    asset.set(md5_hexdigest: nil)
+
+    worker.perform(asset.id.to_s)
+
+    expect(asset.reload.md5_hexdigest).to eq(asset.md5_hexdigest_from_file)
+  end
+end
