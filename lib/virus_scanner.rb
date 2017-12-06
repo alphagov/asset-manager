a925134cc1e09ec546d18ff569bae58e2e10b815
James Mead <james@floehopper.org>
2017-12-06 22:10:27 +0000

    Fix violation of Style/CommentedKeyword cop

--- lib/virus_scanner.rb
+++ lib/virus_scanner.rb
@@ -6,7 +6,9 @@ require 'open3'
 # to either clamscan or clamdscan
 class VirusScanner
   class Error < StandardError; end
-  class InfectedFile < StandardError; end # Used for sending exception notices on infection
+
+  # Used for sending exception notices on infection
+  class InfectedFile < StandardError; end
 
   def initialize(file_path)
     @file_path = file_path
