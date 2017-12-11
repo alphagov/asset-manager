22ae695b909d4001bb1e650d96608b4d594c7e14
James Mead <james@floehopper.org>
2017-12-11 12:18:29 +0000

    Simplify GovukConfiguration#app_host
    AssetManager.app_host is already being set to the value returned from
    this method in config/initializers/govuk.rb. Setting inside the method
    was a mistake and could have led to confusion.

--- lib/govuk_configuration.rb
+++ lib/govuk_configuration.rb
@@ -6,8 +6,8 @@ class GovukConfiguration
   def app_host
     app_name = @env.fetch('GOVUK_APP_NAME')
     app_domain = @env.fetch('GOVUK_APP_DOMAIN')
-    AssetManager.app_host = "http://#{app_name}.#{app_domain}"
+    "http://#{app_name}.#{app_domain}"
   rescue KeyError
-    AssetManager.app_host = 'http://localhost:3000'
+    'http://localhost:3000'
   end
 end
