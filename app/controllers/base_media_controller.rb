ad41fedbfac0cbd338eda9135441e80fa48ca14b
Chris Roos <chris@seagul.co.uk>
2017-12-14 16:53:05 +0000

    DRY up BaseMediaController#proxy_to_s3_via_nginx
    Setting the response headers earlier means that I can use their values
    for `ETag` and `Last-Modified` instead of reconstructing them.

--- app/controllers/base_media_controller.rb
+++ app/controllers/base_media_controller.rb
@@ -4,23 +4,25 @@ class BaseMediaController < ApplicationController
 protected
 
   def proxy_to_s3_via_nginx(asset)
+    headers['ETag'] = %{"#{asset.etag}"}
+    headers['Last-Modified'] = asset.last_modified.httpdate
+    headers['Content-Disposition'] = AssetManager.content_disposition.header_for(asset)
+
     last_modified_from_request = request.headers['If-Modified-Since']
     etag_from_request = request.headers['If-None-Match']
 
     request_is_fresh = false
     if last_modified_from_request || etag_from_request
       request_is_fresh = true
-      request_is_fresh &&= (Time.parse(last_modified_from_request) >= asset.last_modified) if last_modified_from_request
-      request_is_fresh &&= (etag_from_request == %{"#{asset.etag}"}) if etag_from_request
+      request_is_fresh &&= (Time.parse(last_modified_from_request) >= Time.parse(headers['Last-Modified'])) if last_modified_from_request
+      request_is_fresh &&= (etag_from_request == headers['ETag']) if etag_from_request
     end
 
     unless request_is_fresh
       url = Services.cloud_storage.presigned_url_for(asset, http_method: request.request_method)
       headers['X-Accel-Redirect'] = "/cloud-storage-proxy/#{url}"
     end
-    headers['ETag'] = %{"#{asset.etag}"}
-    headers['Last-Modified'] = asset.last_modified.httpdate
-    headers['Content-Disposition'] = AssetManager.content_disposition.header_for(asset)
+
     head :ok, content_type: asset.content_type
   end
 end
