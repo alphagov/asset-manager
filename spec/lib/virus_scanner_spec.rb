d31ae7681e910f2d49aed50b0ab8bcdf28ab663b
James Mead <james@floehopper.org>
2017-12-29 13:21:44 +0000

    DRY up VirusScanner spec

--- spec/lib/virus_scanner_spec.rb
+++ spec/lib/virus_scanner_spec.rb
@@ -3,44 +3,46 @@ require 'virus_scanner'
 
 RSpec.describe VirusScanner do
   describe "scanning a file" do
-    subject(:scanner) { described_class.new("/path/to/file") }
+    subject(:scanner) { described_class.new(file_path) }
+
+    let(:file_path) { '/path/to/file' }
 
     it "calls out to clamdscan" do
       status = double("Process::Status", exitstatus: 0)
-      expect(Open3).to receive(:capture2e).with("govuk_clamscan", "--no-summary", "/path/to/file").and_return(["", status])
+      expect(Open3).to receive(:capture2e).with("govuk_clamscan", "--no-summary", file_path).and_return(["", status])
 
       scanner.scan
     end
 
     it "returns true if clamdscan detects no virus" do
       status = double("Process::Status", exitstatus: 0)
-      allow(Open3).to receive(:capture2e).and_return(["/path/to/file: OK", status])
+      allow(Open3).to receive(:capture2e).and_return(["#{file_path}: OK", status])
 
       expect(scanner.scan).to eq(true)
     end
 
     it "returns false if clamdscan detects a virus" do
       status = double("Process::Status", exitstatus: 1)
-      allow(Open3).to receive(:capture2e).and_return(["/path/to/file: Eicar-Test-Signature FOUND", status])
+      allow(Open3).to receive(:capture2e).and_return(["#{file_path}: Eicar-Test-Signature FOUND", status])
 
       expect(scanner.scan).to eq(false)
     end
 
     it "makes virus info available after detecting a virus" do
       status = double("Process::Status", exitstatus: 1)
-      allow(Open3).to receive(:capture2e).and_return(["/path/to/file: Eicar-Test-Signature FOUND", status])
+      allow(Open3).to receive(:capture2e).and_return(["#{file_path}: Eicar-Test-Signature FOUND", status])
 
       scanner.scan
-      expect(scanner.virus_info).to eq("/path/to/file: Eicar-Test-Signature FOUND")
+      expect(scanner.virus_info).to eq("#{file_path}: Eicar-Test-Signature FOUND")
     end
 
     it "raises an error with the output message if clamdscan fails" do
       status = double("Process::Status", exitstatus: 2)
-      allow(Open3).to receive(:capture2e).and_return(["ERROR: Can't access file /path/to/file", status])
+      allow(Open3).to receive(:capture2e).and_return(["ERROR: Can't access file #{file_path}", status])
 
       expect {
         scanner.scan
-      }.to raise_error(VirusScanner::Error, "ERROR: Can't access file /path/to/file")
+      }.to raise_error(VirusScanner::Error, "ERROR: Can't access file #{file_path}")
     end
   end
 end
