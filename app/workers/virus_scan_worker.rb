b7fb12117d9281975a85e4366ffe89d980d1c23c
James Mead <james@floehopper.org>
2018-02-02 15:11:18 +0000

    Introduce begin/end block into VirusScanWorker#perform
    I'm about to introduce an `unless` statement into this method. By making
    this change first, it'll be easier to see what's going on.

--- app/workers/virus_scan_worker.rb
+++ app/workers/virus_scan_worker.rb
@@ -5,10 +5,12 @@ class VirusScanWorker
 
   def perform(asset_id)
     asset = Asset.find(asset_id)
-    Services.virus_scanner.scan(asset.file.path)
-    asset.scanned_clean!
-  rescue VirusScanner::InfectedFile => e
-    GovukError.notify(e, extra: { id: asset.id, filename: asset.filename })
-    asset.scanned_infected!
+    begin
+      Services.virus_scanner.scan(asset.file.path)
+      asset.scanned_clean!
+    rescue VirusScanner::InfectedFile => e
+      GovukError.notify(e, extra: { id: asset.id, filename: asset.filename })
+      asset.scanned_infected!
+    end
   end
 end
