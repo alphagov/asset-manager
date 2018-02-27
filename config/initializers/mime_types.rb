f1c8bf9271d594883fa7258a43015ce48277a38a
Chris Lowis <chris.lowis@gmail.com>
2018-02-27 05:38:38 -0500

    Sort entries in mime_types.rb

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,13 +3,10 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 
-Mime::Type.register "text/rtf", :rtf
-
-Mime::Type.register "application/vnd.ms-excel", :xls
 Mime::Type.register "application/msword", :doc
-
-Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
-Mime::Type.register "application/vnd.openxmlformats-officedocument.wordprocessingml.document", :docx
-
+Mime::Type.register "application/vnd.ms-excel", :xls
 Mime::Type.register "application/vnd.oasis.opendocument.spreadsheet", :ods
 Mime::Type.register "application/vnd.oasis.opendocument.text", :odt
+Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
+Mime::Type.register "application/vnd.openxmlformats-officedocument.wordprocessingml.document", :docx
+Mime::Type.register "text/rtf", :rtf
