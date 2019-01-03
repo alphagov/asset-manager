cd4fe463f8d009294d004f50f0d8368877308512
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-01-03 11:06:29 +0000

    Fix indentation linting issues

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
-if Rails.env.test?
-  directory = "#{Rails.root}/tmp/test_uploads"
-else
-  directory = "#{ENV['GOVUK_APP_ROOT'] || Rails.root}/uploads"
-end
+directory = if Rails.env.test?
+              "#{Rails.root}/tmp/test_uploads"
+            else
+              "#{ENV['GOVUK_APP_ROOT'] || Rails.root}/uploads"
+            end
 
 AssetManager.carrier_wave_store_base_dir = directory
