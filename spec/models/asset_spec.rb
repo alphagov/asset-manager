bde0281262624f990b5e18ce45cf11698dd3abc9
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-07-24 14:32:21 +0100

    Adds access_limited_organisation_ids to assets
    This adds an extra field and logic to the asset model to support
    access limiting by user organisation id.

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -217,7 +217,7 @@ RSpec.describe Asset, type: :model do
     end
 
     it 'returns true if the asset is draft but not access limited' do
-      asset = FactoryBot.build(:asset, draft: true, access_limited: [])
+      asset = FactoryBot.build(:asset, draft: true)
       expect(asset).to be_accessible_by(nil)
     end
 
@@ -230,6 +230,18 @@ RSpec.describe Asset, type: :model do
     it 'returns false if the asset is draft and access limited and the user is not authorised to view it' do
       user = FactoryBot.build(:user, uid: 'user-id')
       asset = FactoryBot.build(:asset, draft: true, access_limited: ['another-user-id'])
+      expect(asset).not_to be_accessible_by(user)
+    end
+
+    it 'returns true if the asset is draft and access limited and the users org is authorised to view it' do
+      user = FactoryBot.build(:user, uid: 'user-id', organisation_content_id: 'org-id')
+      asset = FactoryBot.build(:asset, draft: true, access_limited_organisation_ids: ['org-id'])
+      expect(asset).to be_accessible_by(user)
+    end
+
+    it 'returns false if the asset is draft and access limited and the users org is not authorised to view it' do
+      user = FactoryBot.build(:user, uid: 'user-id', organisation_content_id: 'org-id-A')
+      asset = FactoryBot.build(:asset, draft: true, access_limited_organisation_ids: ['org-id-B'])
       expect(asset).not_to be_accessible_by(user)
     end
   end
