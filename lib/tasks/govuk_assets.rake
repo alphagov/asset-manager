alphagov/asset-manager

commit d2897ec463e3efdc3139a5c3d94257bc9a46ce31

Remove top-level methods in rake tasks
This makes 'create_or_replace_whitehall_asset' into a generic method
of the WhitehallAsset model, and duplicates the remaining code between
the tasks, in order to fix the linting issue for top-level methods.

lib/tasks/govuk_assets.rake
@@ -17,31 +17,13 @@ namespace :govuk_assets do
     puts "#{status}: #{result.written_count} documents updated"
   end
 
-  def create_or_replace_whitehall_asset(file_path, legacy_url_path)
-    begin
-      prior = WhitehallAsset.find_by(legacy_url_path: legacy_url_path)
-      prior.file = Pathname.new(file_path).open
-      prior.save!
-    rescue Mongoid::Errors::DocumentNotFound
-      WhitehallAsset.create!(
-        file: Pathname.new(file_path).open,
-        legacy_url_path: legacy_url_path,
-      )
-    end
-    puts "Uploaded '#{file_path}' to '#{legacy_url_path}'"
-  end
-
-  def create_or_replace_hmrc_asset(file_path, basename)
-    hmrc_url_base = "/government/uploads/uploaded/hmrc"
-    create_or_replace_whitehall_asset(file_path, "#{hmrc_url_base}/#{basename}")
-  end
-
   desc "Upload all *.zip files in the given directory to /government/uploads/uploaded/hmrc"
   task :create_hmrc_paye_zips, %i[directory] => :environment do |_, args|
     directory = args[:directory]
+    hmrc_url_base = "/government/uploads/uploaded/hmrc"
 
     Dir.glob(File.join(directory, "*.zip")).each do |file_path|
-      create_or_replace_hmrc_asset(file_path, File.basename(file_path))
+      WhitehallAsset.create_or_replace(file_path, "#{hmrc_url_base}/#{basename}")
     end
   end
 
@@ -54,11 +36,12 @@ namespace :govuk_assets do
       basename = args.extras[0]
     end
 
-    create_or_replace_hmrc_asset(file_path, basename)
+    hmrc_url_base = "/government/uploads/uploaded/hmrc"
+    WhitehallAsset.create_or_replace(file_path, "#{hmrc_url_base}/#{basename}")
   end
 
   desc "Create a whitehall asset with the given legacy URL path"
   task :create_whitehall_asset, %i[file_path legacy_url_path] => :environment do |_, args|
-    create_or_replace_whitehall_asset(args[:file_path], args[:legacy_url_path])
+    WhitehallAsset.create_or_replace(args[:file_path], args[:legacy_url_path])
   end
 end

app/models/whitehall_asset.rb
@@ -22,6 +22,17 @@ class WhitehallAsset < Asset
     order(updated_at: :desc).find_by(legacy_url_path: legacy_url_path)
   end
 
+  def self.create_or_replace(file_path, legacy_url_path)
+    prior = WhitehallAsset.find_by(legacy_url_path: legacy_url_path)
+    prior.file = Pathname.new(file_path).open
+    prior.save!
+  rescue Mongoid::Errors::DocumentNotFound
+    WhitehallAsset.create!(
+      file: Pathname.new(file_path).open,
+      legacy_url_path: legacy_url_path,
+    )
+  end
+
   def etag
     legacy_etag || super
   end

commit f0d78626017671d377449e68d14f1d2a694e9c0e

Fix HMRC .zip upload rake task
This was attempted in d2897ec463e3efdc3139a5c3d94257bc9a46ce31, but didn't
quite correct the bug introduced by the refactor in d2897ec463e3efdc3139a5c3d94257bc9a46ce31.
The original code was 'create_or_replace_hmrc_asset(file_path, File.basename(file_path))',
so I think this commit will fix it for good.

lib/tasks/govuk_assets.rake
@@ -23,7 +23,7 @@ namespace :govuk_assets do
     hmrc_url_base = "/government/uploads/uploaded/hmrc"
 
     Dir.glob(File.join(directory, "*.zip")).each do |file_path|
-      WhitehallAsset.create_or_replace(file_path, "#{hmrc_url_base}/#{file_path}")
+      WhitehallAsset.create_or_replace(file_path, "#{hmrc_url_base}/#{File.basename(file_path)}")
     end
   end
 