a45e42f8504e0e1aab6fae3b3d2e3b1b6b40a8d6
Michael Walker <mike@barrucadu.co.uk>
2021-04-19 15:28:32 +0100

    Fix readiness healthcheck path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
-  get "/healthcheck", to: GovukHealthcheck.rack_response(
+  get "/healthcheck/ready", to: GovukHealthcheck.rack_response(
     GovukHealthcheck::Mongoid,
     GovukHealthcheck::SidekiqRedis,
   )
