9ddd0ef2394c26c6b48b642feefa3be2b1606a5f
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2023-11-01 15:27:40 +0000

    Run `rails update`

--- config/application.rb
+++ config/application.rb
@@ -2,17 +2,16 @@ require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
-# require "active_model/railtie"
-# require "active_job/railtie"
+require "active_model/railtie"
+require "active_job/railtie"
 # require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-# require "action_view/railtie"
+require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -24,6 +23,11 @@ module AssetManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -32,28 +36,7 @@ module AssetManager
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # This app frequently redirects to different hosts (GOV.UK redirects,
-    # between draft and live hosts).
-    config.action_controller.raise_on_open_redirects = false
-
-    # Disable Rack::Cache
-    config.action_dispatch.rack_cache = nil
-
-    config.assets.prefix = "/asset-manager"
-
-    unless Rails.application.secrets.jwt_auth_secret
-      raise "JWT auth secret is not configured. See config/secrets.yml"
-    end
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
-
-  mattr_accessor :govuk
-
-  mattr_accessor :s3
-
-  mattr_accessor :carrier_wave_store_base_dir
-
-  mattr_accessor :content_disposition
-  mattr_accessor :default_content_type
-
-  mattr_accessor :fake_s3
 end
