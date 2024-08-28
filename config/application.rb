fe8b0e6d3d1ad3f686a1d0b6af0fa2a12349610b
David Mays <david.mays@digital.cabinet-office.gov.uk>
2024-08-28 15:26:08 +0100

    Add Actionlint, upgrade to Ruby 3.3.4

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module AssetManager
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    # config.govuk_time_zone = "London"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
