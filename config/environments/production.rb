619c5ddc4d74dc5fabd05dd2dc1426e4219dae58
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-01-20 17:19:24 +0000

    Adopt some config that our other apps use
    Before I get into updating some config following the upgrade to Rails 8,
    I spotted a couple of production config settings that don't line up with
    the other apps that I've been working on.
    
    I don't think this app has any need for serving static files in
    production. I haven't found any explanation for why it was enabled when
    it was or any evidence that it was ever enabled using Rails's old
    `RAILS_SERVE_STATIC_FILES` environment variable before that.
    
    We've enabled the request_id log tag in the other apps I've been looking
    into and I didn't find any explanation for why that wasn't adopted here,
    so I'm giving it a go for consistency.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = false
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -52,7 +52,7 @@ Rails.application.configure do
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
-  # config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
