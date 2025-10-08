5bd6907483d3b3b2c3bc184f3f0eb05fb0277642
Bruce Bolt <bruce.bolt@dsit.gov.uk>
2025-10-08 10:09:46 +0100

    Remove `BaseAssetsController`
    This is now only being used by the `AssetsController`, so doesn't need
    to be a separate controller anymore.
    
    Therefore moving the controller code into the `AssetsController`.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,5 +1,22 @@
-class AssetsController < BaseAssetsController
+class AssetsController < ApplicationController
   before_action :restrict_request_format
+
+  def show
+    @asset = find_asset(include_deleted: true)
+
+    expires_now
+    render json: AssetPresenter.new(@asset, view_context)
+  end
+
+  def create
+    @asset = build_asset
+
+    if @asset.save
+      render json: AssetPresenter.new(@asset, view_context).as_json(status: :created), status: :created
+    else
+      error 422, @asset.errors.full_messages
+    end
+  end
 
   def update
     @asset = Asset.undeleted.or(Asset.where(draft: true)).find(params[:id])
@@ -30,7 +47,27 @@ private
   end
 
   def asset_params
-    base_asset_params.permit(
+    params.require(:asset).tap { |asset|
+      if asset.key?(:redirect_url) && asset[:redirect_url].blank?
+        asset[:redirect_url] = nil
+      end
+
+      if asset.key?(:access_limited_user_ids)
+        asset[:access_limited] = asset[:access_limited_user_ids]
+      end
+
+      if asset.key?(:access_limited) && asset[:access_limited].empty?
+        asset[:access_limited] = []
+      end
+
+      if asset.key?(:access_limited_organisation_ids) && asset[:access_limited_organisation_ids].empty?
+        asset[:access_limited_organisation_ids] = []
+      end
+
+      if asset.key?(:auth_bypass_ids) && asset[:auth_bypass_ids].empty?
+        asset[:auth_bypass_ids] = []
+      end
+    }.permit(
       :file,
       :draft,
       :redirect_url,
