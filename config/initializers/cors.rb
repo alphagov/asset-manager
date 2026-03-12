50cd511e975db7f180763c0550fe88527d5fd42e
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2026-03-12 16:33:49 +0000

    Temporary debugging commit to check if CORS will work at all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Plek.find("whitehall-admin", external: true)
+    origins "*"
 
-    resource "/media/*",
+    resource "*",
              headers: :any,
              methods: [:get]
   end
