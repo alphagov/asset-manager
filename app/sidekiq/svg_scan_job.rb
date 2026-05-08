38113bb4ac3a1c7f243079332bdf6de3d3dddced
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2026-05-08 16:03:56 +0000

    Add SvgScanJob, Scanning logic and appropriate testing
    The scanning relies on the `Nokogiri::XML::SAX::Parser`, which requires
    a `start_element` method to be implemented and passed in, this also includes
    a constant of unsafe_elements that the parser will look for.
    
    Also includes a change to Rspec config, as the `file_handler` config
    is much more appropriate for this use case.
    
    Integration with the Asset model state machine will come in a
    subsequent commit. Following that, another commit will introduce tests
    of the SvgScanJob and its integration with the state machine.

---
+++ app/sidekiq/svg_scan_job.rb
@@ -0,0 +1,23 @@
+require "services"
+
+class SvgScanJob
+  include Sidekiq::Job
+  include EnsureFile
+
+  sidekiq_options lock: :until_executing
+
+  def perform(asset_id)
+    asset = Asset.find(asset_id)
+    if asset.scanned_clean?
+      begin
+        Rails.logger.info("#{asset_id} - SvgScanJob#perform - SVG scan started")
+        ensure_file_is_same_after_scan(asset, "SvgScanJob", :svg_scanned_clean!) do
+          Services.svg_scanner.scan(asset.file.path)
+        end
+      rescue SvgDocument::UnsafeSvg => e
+        GovukError.notify(e, extra: { id: asset.id, filename: asset.filename })
+        asset.scanned_infected!
+      end
+    end
+  end
+end
