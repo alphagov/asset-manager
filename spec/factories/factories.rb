9d8d2d0d06f713272b0c898b8175def753d828ac
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2026-06-09 19:51:28 +0000

    Test SvgScanJob
    These are adapted from the spec of VirusScanJob. Those tests include
    things like "when the asset is already marked as uploaded it does not
    scan file", even though it's the asset models' duty to control whether
    the scanning job is performed on an asset in a given state. Because the
    tests exist, the scan jobs themselves have to check the state of the
    asset. Such tests are only included here for consistency, but one day
    the SVG and Virus scan job specs should both be refactored.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -17,11 +17,13 @@ FactoryBot.define do
 
   factory :svg_asset_safe, parent: :asset do
     file { Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/files/asset-safe.svg")) }
+    after :create do |asset|
+      asset.virus_scanned_clean!
+    end
   end
 
   factory :svg_asset_clean, parent: :svg_asset_safe do
     after :create do |asset|
-      asset.virus_scanned_clean!
       asset.svg_scanned_clean!
     end
   end
@@ -36,6 +38,14 @@ FactoryBot.define do
 
   factory :svg_asset_unsafe_uri, parent: :asset do
     file { Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/files/asset-unsafe-uri.svg")) }
+  end
+
+  factory :svg_infected_asset, parent: :svg_asset_safe do
+    after :create, &:scanned_infected!
+  end
+
+  factory :svg_uploaded_asset, parent: :svg_asset_clean do
+    after :create, &:upload_success!
   end
 
   factory :deleted_asset, parent: :asset do
