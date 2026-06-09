1b72271b799b6d932a12bd0be20cf5d52119648c
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2026-06-09 20:49:06 +0000

    Fixup d91d974dcdef3caebd6dbd9bf4c85fdd5e88d4ab

--- spec/lib/svg_scanner_spec.rb
+++ spec/lib/svg_scanner_spec.rb
@@ -9,31 +9,25 @@ RSpec.describe SvgScanner, type: :file_handler do
     let(:unsafe_element_file_path) { fixture_file_path("asset-unsafe-element.svg") }
     let(:unsafe_event_handler_file_path) { fixture_file_path("asset-unsafe-event-handler.svg") }
     let(:unsafe_uri_file_path) { fixture_file_path("asset-unsafe-uri.svg") }
-    let(:nokogiri_parser) { instance_double(Nokogiri::XML::SAX::Parser) }
-
-    before do
-      allow(Nokogiri::XML::SAX::Parser).to receive(:new).and_return(nokogiri_parser)
-    end
 
     it "calls out to Nokogiri" do
-      allow(nokogiri_parser).to receive(:parse).with(File.read(safe_file_path))
+      nokogiri_parser = instance_double(Nokogiri::XML::SAX::Parser)
+      allow(Nokogiri::XML::SAX::Parser).to receive(:new).and_return(nokogiri_parser)
+      allow(nokogiri_parser).to receive(:parse_io).with(an_object_having_attributes(path: safe_file_path))
 
-      expect(nokogiri_parser).to receive(:parse).with(File.read(safe_file_path))
+      expect(nokogiri_parser).to receive(:parse_io)
+
       scanner.scan(safe_file_path)
     end
 
     context "when SVG is safe" do
       it "returns true" do
-        allow(nokogiri_parser).to receive(:parse).with(File.read(safe_file_path))
-
         expect(scanner.scan(safe_file_path)).to be true
       end
     end
 
     context "when SVG contains an unsafe element" do
       it "raises a relevant error" do
-        allow(nokogiri_parser).to receive(:parse).with(File.read(unsafe_element_file_path)).and_raise(SvgDocument::UnsafeSvg, "SVG: Unsafe element detected: <script>")
-
         expect {
           scanner.scan(unsafe_element_file_path)
         }.to raise_error(
@@ -44,8 +38,6 @@ RSpec.describe SvgScanner, type: :file_handler do
 
     context "when SVG contains an unsafe event handler" do
       it "raises a relevant error" do
-        allow(nokogiri_parser).to receive(:parse).with(File.read(unsafe_event_handler_file_path)).and_raise(SvgDocument::UnsafeSvg, "SVG: Unsafe event handler detected: 'onload' on <svg>")
-
         expect {
           scanner.scan(unsafe_event_handler_file_path)
         }.to raise_error(
@@ -56,8 +48,6 @@ RSpec.describe SvgScanner, type: :file_handler do
 
     context "when SVG contains an unsafe URI" do
       it "raises a relevant error" do
-        allow(nokogiri_parser).to receive(:parse).with(File.read(unsafe_uri_file_path)).and_raise(SvgDocument::UnsafeSvg, "SVG: Unsafe javascript URI detected in 'href' attribute on <a>")
-
         expect {
           scanner.scan(unsafe_uri_file_path)
         }.to raise_error(
