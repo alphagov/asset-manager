3b3345899f8c52228f4f2c01684e706797efef82
Patrick Cartlidge <patrick.cartlidge@digital.cabinet-office.gov.uk>
2026-08-19 16:00:20 +0100

    change cookie same_site

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_asset-manager_session"
+Rails.application.config.session_store :cookie_store,
+  key: "_asset_manager_session",
+  same_site: :none,
+  secure: true,
+  httponly: true
